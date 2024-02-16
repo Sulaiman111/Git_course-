bb6b1a323144b8be1f366564fd58cab7e5ac25f3
Sulaiman Kurdi <sulaimankurdi@Sulaimans-MacBook-Pro.local>
2024-02-16 22:51:53 +0200

    update the javascript file

--- js/index.js
+++ js/index.js
@@ -1,17 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <title></title>
-  <meta name="Generator" content="Cocoa HTML Writer">
-  <meta name="CocoaVersion" content="2487.3">
-  <style type="text/css">
-    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Times; -webkit-text-stroke: #000000; min-height: 14.0px}
-    span.s1 {font-kerning: none}
-  </style>
-</head>
-<body>
-<p class="p1"><span class="s1"></span><br></p>
-</body>
-</html>
+
+// Define a function to add two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Use the function to add two numbers
+var sum = addNumbers(5, 3);
+
+// Display the result in the console
+console.log('The sum of 5 and 3 is:', sum);
